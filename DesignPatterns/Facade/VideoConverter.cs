059471bf2fbb468364390ba780437e5dfa3e1503
Eugenia Los <eugenia.los99@gmail.com>
2019-09-16 18:12:46 +0300

    Facde

---
+++ DesignPatterns/Facade/VideoConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facade
+{
+    static class VideoConverter
+    {
+        public static void Convert(VideoFile file, Codec codec)
+        {
+
+        }
+    }
+}
---
+++ DesignPatterns/Facade/VideoFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facade
+{
+    class VideoFile
+    {
+        string FileName { get; set; }
+        public VideoFile(string filename)
+        {
+            FileName = filename;
+        }
+    }
+}
