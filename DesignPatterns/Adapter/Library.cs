dbcfd47c371e463318c82b24d695253caa7c3877
Eugenia Los <eugenia.los99@gmail.com>
2019-09-16 18:25:19 +0300

    Adapter

---
+++ DesignPatterns/Adapter/Library.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+
+namespace Adapter
+{
+    static class Library
+    {
+        public static void GetBoooksXml()
+        {
+           
+        }
+    }
+}
