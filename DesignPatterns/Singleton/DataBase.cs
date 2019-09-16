5449c4107b068d5f55acdbb329332ece290e3ab6
Eugenia Los <eugenia.los99@gmail.com>
2019-09-16 17:26:57 +0300

    Singleton

---
+++ DesignPatterns/Singleton/DataBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class DataBase
+    {
+        private static DataBase instance;
+        private DataBase()
+        {
+
+        }
+
+        public DataBase getInstance()
+        {
+            if(instance == null)
+            {
+                instance = new DataBase();
+            }
+
+            return instance;
+        }
+    }
+}
