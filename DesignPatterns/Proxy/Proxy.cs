5632637842cf4e6db72e426d684baabe0c4b0524
Eugenia Los <eugenia.los99@gmail.com>
2019-09-17 09:37:12 +0300

    Proxy

---
+++ DesignPatterns/Proxy/Proxy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proxy
+{
+    class Proxy: IYersterdayRate
+    {
+        public void getRate()
+        {
+            YersterdayRate rate = new YersterdayRate();
+            rate.getRate();
+        }
+
+    }
+}
