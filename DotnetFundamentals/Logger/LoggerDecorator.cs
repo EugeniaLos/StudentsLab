d2bcf8f45074370dadf87cd1f0ab76cc42401a90
Eugenia Los <eugenia.los99@gmail.com>
2019-09-23 13:36:29 +0300

    Add LoggerDecorator

---
+++ DotnetFundamentals/Logger/LoggerDecorator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logger
+{
+    class LoggerDecorator: ILogger
+    {
+        protected ILogger Wrapee;
+
+        public LoggerDecorator(ILogger source)
+        {
+            Wrapee = source;
+        }
+
+        public void Error(string message)
+        {
+            Wrapee.Error(message);
+        }
+
+        public void Error(Exception ex)
+        {
+            Wrapee.Error(ex);
+        }
+
+        public void Warning(string message)
+        {
+            Wrapee.Warning(message);
+        }
+
+        public void Info(string message)
+        {
+            Wrapee.Info(message);
+        }
+    }
+}
