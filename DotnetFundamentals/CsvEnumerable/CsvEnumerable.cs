a20babf98a50e6995956cf4ed5f41df407faaffa
Eugenia Los <eugenia.los99@gmail.com>
2019-09-30 12:49:12 +0300

    Implement non-generic CsvEnumerable

---
+++ DotnetFundamentals/CsvEnumerable/CsvEnumerable.cs
@@ -0,0 +1,70 @@
+﻿using CsvHelper;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace CsvEnumerable
+{
+    public class CsvEnumerable: IEnumerable
+    {
+        private List<string> records = new List<string>();
+        public CsvEnumerable(string fileName)
+        {
+            using (StreamReader sr = new StreamReader(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, fileName)))
+            {
+                String line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    records.Add(line);
+                }
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new CsvEnumarator(records);
+        }
+
+        private class CsvEnumarator: IEnumerator
+        {
+            private string[] records;
+            private int position = -1;
+
+            public CsvEnumarator(List<string> records)
+            {
+                this.records = new string[records.Count];
+                records.CopyTo(this.records);
+            }
+
+            public object Current
+            {
+                get
+                {
+                    if (position != -1 || position < records.Length)
+                        return records[position];
+                    return "No record";
+                }
+            }
+
+            public bool MoveNext()
+            {
+                if (position < records.Length - 1)
+                {
+                    position++;
+                    return true;
+                }
+                else
+                    return false;
+            }
+
+            public void Reset()
+            {
+                position = -1;
+            }
+        }
+    }
+}
---
+++ DotnetFundamentals/CsvEnumerable/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CsvEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var data = new CsvEnumerable("File.csv");
+            foreach(string record in data)
+            {
+                Console.WriteLine(record);
+            }
+        }
+    }
+}
