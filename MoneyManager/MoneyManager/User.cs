c7e6c06a2c81df24cc238df8097de325ffe5b708
Eugenia Los <eugenia.los99@gmail.com>
2019-10-01 13:47:12 +0300

    Add user class

---
+++ MoneyManager/MoneyManager/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoneyManager
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
