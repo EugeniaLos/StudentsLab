a85e3cb7bffa9154230d80f218725557bf0e1759
Eugenia Los <eugenia.los99@gmail.com>
2019-10-15 15:46:48 +0300

    Add migration rmv ParentId and transform transactn

---
+++ MoneyManager/MoneyManager/Migrations/20191015102017_DeleteColumn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoneyManager.Migrations
+{
+    public partial class DeleteColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var b = migrationBuilder.Sql(@"UPDATE dbo.Transactions SET dbo.Transactions.CategoryId = dbo.Categories.ParentId FROM dbo.Transactions INNER JOIN dbo.Categories ON dbo.Transactions.CategoryId = dbo.Categories.Id WHERE dbo.Categories.Id IN (SELECT dbo.Categories.Id FROM dbo.Categories WHERE dbo.Categories.ParentId != 0)");
+            migrationBuilder.DropColumn("ParentId", "dbo.Categories");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
