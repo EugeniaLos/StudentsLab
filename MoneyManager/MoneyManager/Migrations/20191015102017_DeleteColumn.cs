b44ea073e3198f0373d73348a8c3b4e8c87e3d71
Eugenia Los <eugenia.los99@gmail.com>
2019-10-15 18:11:58 +0300

    edit DeleteColumn

--- MoneyManager/MoneyManager/Migrations/20191015102017_DeleteColumn.cs
+++ MoneyManager/MoneyManager/Migrations/20191015102017_DeleteColumn.cs
@@ -8,7 +8,12 @@ namespace MoneyManager.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            var b = migrationBuilder.Sql(@"UPDATE dbo.Transactions SET dbo.Transactions.CategoryId = dbo.Categories.ParentId FROM dbo.Transactions INNER JOIN dbo.Categories ON dbo.Transactions.CategoryId = dbo.Categories.Id WHERE dbo.Categories.Id IN (SELECT dbo.Categories.Id FROM dbo.Categories WHERE dbo.Categories.ParentId != 0)");
+            var b = migrationBuilder.Sql(@"UPDATE dbo.Transactions SET dbo.Transactions.CategoryId = dbo.Categories.ParentId
+                                           FROM dbo.Transactions
+                                           INNER JOIN dbo.Categories
+                                           ON dbo.Transactions.CategoryId = dbo.Categories.Id
+                                           WHERE dbo.Categories.Id
+                                           IN (SELECT dbo.Categories.Id FROM dbo.Categories WHERE dbo.Categories.ParentId != 0)");
             migrationBuilder.DropColumn("ParentId", "dbo.Categories");
         }
 
