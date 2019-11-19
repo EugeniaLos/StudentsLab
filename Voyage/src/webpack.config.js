380273193cb99ffe79b4511284a50822cd99d0a5
Eugenia Los <eugenia.los99@gmail.com>
2019-11-19 14:38:30 +0300

    add scss variables

--- Voyage/src/webpack.config.js
+++ Voyage/src/webpack.config.js
@@ -14,7 +14,16 @@ module.exports = {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"]
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass")
+            }
+          }
+        ]
       },
       {
         test: /\.css$/i,
