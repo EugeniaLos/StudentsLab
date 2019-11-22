7982a8c8ab1d0267710fdf4f8e63a90553401083
Eugenia Los <eugenia.los99@gmail.com>
2019-11-22 12:20:43 +0300

    implement all functions

--- ArrayLib/arrayLib.js
+++ ArrayLib/arrayLib.js
@@ -20,7 +20,27 @@ let arrayLib = {
     }
     return output;
   },
-  reduce(arr, callback, initialValue) {},
-  filter(array, callback) {},
-  foreach(array, callback) {}
+  reduce(arr, callback, initialValue) {
+    let secondArgument = initialValue;
+    for (let i = 0; i < arr.length; i++) {
+      secondArgument = callback(arr[i], secondArgument);
+    }
+    return secondArgument;
+  },
+  filter(arr, callback) {
+    let output = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (callback(arr[i])) {
+        output.push(arr[i]);
+      }
+    }
+    return output;
+  },
+  foreach(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] != null) {
+        callback(arr[i]);
+      }
+    }
+  }
 };
