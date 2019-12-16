e9051968648dc127eef4066e05461062ef78a714
Eugenia Los <eugenia.los99@gmail.com>
2019-12-16 16:57:17 +0300

    add redux basic structure

--- YouTubeVideoSearch/src/components/Search.js
+++ YouTubeVideoSearch/src/components/Search.js
@@ -3,12 +3,20 @@ import React, { Component } from "react";
 class Search extends Component {
   state = { searchText: null };
 
+  onChange = evt => this.setState({ searchText: evt.target.value });
+
+  // onBlur = e => console.log(e.target.value);
+
   render() {
+    // const { fetchYoutubeVideo } = this.props;
+    console.log(this.state.searchText);
+
     return (
       <input
         className="search"
         type="text"
         placeholder="Search"
+        onBlur={this.onBlur}
         value={this.state.searchText}
       ></input>
     );
