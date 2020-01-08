c7e9e8ebf6120ded58bb66d558af1ccb1a1b907d
Eugenia Los <eugenia.los99@gmail.com>
2020-01-08 18:43:57 +0300

    init commit

---
+++ AspNetProject/AspNetProject/Controllers/VideoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace AspNetProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VideoController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<VideoController> _logger;
+
+        public VideoController(ILogger<VideoController> logger)
+        {
+            _logger = logger;
+        }
+
+        //public IEnumerable<> Gett()
+        //{
+
+        //}
+
+        [HttpGet]
+        public string Get()
+        {
+            //WebClient client = new WebClient();
+            //string reply = client.DownloadString("");
+            return "hi";
+        }
+    }
+}
---
+++ AspNetProject/AspNetProject/Video.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetProject
+{
+    public class Video
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ThumbnailUrl { get; set; }
+
+        //public Video(string id, string title, string description, string thumbnailUrl)
+        //{
+        //    this.Id = id;
+        //    this.Title = title;
+        //    this.Description = description;
+        //    this.ThumbnailUrl = thumbnailUrl;
+        //}
+    }
+}
