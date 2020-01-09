fd25ab197155b56fef619e51233416901a12f41d
Eugenia Los <eugenia.los99@gmail.com>
2020-01-09 16:12:49 +0300

    implement youtube search

--- AspNetProject/AspNetProject/Controllers/VideoController.cs
+++ AspNetProject/AspNetProject/Controllers/VideoController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Net;
+using Microsoft.Extensions.Configuration;
 
 namespace AspNetProject.Controllers
 {
@@ -12,29 +13,39 @@ namespace AspNetProject.Controllers
     [Route("[controller]")]
     public class VideoController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
         private readonly ILogger<VideoController> _logger;
 
-        public VideoController(ILogger<VideoController> logger)
+        private readonly IConfiguration _configuration;
+
+        public VideoController(ILogger<VideoController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
-
-        //public IEnumerable<> Gett()
-        //{
-
-        //}
 
         [HttpGet]
         public string Get()
         {
-            //WebClient client = new WebClient();
-            //string reply = client.DownloadString("");
-            return "hi";
+            return "There is no search filters";
+        }
+
+        [HttpGet("{searchString}")]
+        public string Get(string searchString)
+        {
+            string KEY = _configuration.GetSection("YoutubeApiKey").Value;
+            WebClient client = new WebClient();
+            string reply = client.DownloadString($"https://www.googleapis.com/youtube/v3/search?part=snippet&key={KEY}&q={searchString}&type=video");
+            return reply;
+        }
+
+        [HttpGet("{searchString}/{count}")]
+        public string Get(string searchString,int count)
+        {
+            string KEY = _configuration.GetSection("YoutubeApiKey").Value;
+            WebClient client = new WebClient();
+            string reply = client.DownloadString($"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults={count}&key={KEY}&q={searchString}&type=video");
+            return reply;
         }
     }
 }
